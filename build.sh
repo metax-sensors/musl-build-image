06b2b9fdaf5cd4d402eb2703fa31acaa3849cf4f
Jan Schöppach <jan.schoeppach@metax-gmbh.de>
2026-03-11 10:23:29 +0100

    Update image URLs

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-docker build -t docker.bestsens.local/bone/musl-build-image . || exit 1
-docker run -v ${PWD}:/data -t docker.bestsens.local/bone/musl-build-image \
+docker build -t ghcr.io/metax-sensors/musl-build-image . || exit 1
+docker run -v ${PWD}:/data -t ghcr.io/metax-sensors/musl-build-image \
 		/bin/tar -czvf /data/arm-bemos-linux-musleabihf.tar.gz /opt/x-tools/arm-bemos-linux-musleabihf/
